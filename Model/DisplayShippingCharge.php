5b90fdac73fa31db8595c1ae3466d487a987b99b
anjali <anjali.gupta@ambab.com>
2019-12-23 13:04:33 +0530

    comment added

--- Model/DisplayShippingCharge.php
+++ Model/DisplayShippingCharge.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Ambab SkipShipping Extension
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Ambab
+ * @package     Ambab_SkipShipping
+ * @copyright   Copyright (c) 2019 Ambab (https://www.ambab.com/)
+ * @license     http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
 
 namespace Ambab\SkipShipping\Model;
 
@@ -14,12 +32,26 @@ class DisplayShippingCharge implements ConfigProviderInterface
     ) {
         $this->skipshippingHelper = $skipshippingHelper;
     }
+
+    /**
+     *
+     *  All the admin configuration
+        @return array
+     *
+     */
     public function getConfig()
     {
         $config = [];
         $config['ambabSkipShippingSettings'] = $this->getSettings();
         return $config;
     }
+
+    /**
+     *
+     *  get configuration setting from admin
+        @return array
+     *
+     */
 
     public function getSettings()
     {
